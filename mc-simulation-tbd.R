ae7eb56ca614ae0a1c2743b34b90837505502801
Franziska Günther <Franziska@MacBook-Air-5.fritz.box>
2020-04-13 17:12:23 +0200

    EX 1 from tutorial reconsidered with Gamma distribution and added the respective plot

--- mc-simulation-tbd.R
+++ mc-simulation-tbd.R
@@ -1,49 +1,63 @@
-## This script is supposed to go along with Chris' tutorial on monte carlo simulations and  likelihood. 
+## This script is supposed to go along with a tutorial on monte carlo simulations and maximum likelihood estimators. 
 
-# Q: Why is it 10* √2π and not 4* √2π?
+# Q: Why is it 10 * √2π and not 4 * √2π?
 
-# Goal: Estimating integral 1 up to 3 of N ~ (1, 2) without calculus but with the Monte Carlo simulation method.
+# Goal: Estimating integral 1 up to 4 of Gamma ~ (1, 2) without calculus but with the Monte Carlo simulation method.
 
-# How we will do it: Generate random samples from our normal distribution of which we want to know some integral
-# (some probability), then count the numbers falling into a specific range (that defines the integral), divide them
-# by the total number of samples we draw and there we have the integral.
+# How we will do it: Generate random samples from our gamma distribution of which we want to know some integral
+# (some probability), then count the number of cases within a sample falling into the specified range 
+# (that defines the integral) and divide them by the respective sample size. Compute the mean difference of the
+# repeats from the previously computed accurate integral under the Gamma distribution (mc_exact) and its 
+# standard deviation.
 
-n <- 100 # number of samples to take
-sims <- rnorm(n, mean = 1, sd = 2) # simulated normally distributed numbers
-mc_integral <- sum(sims >= 1 & sims <= 3)/n # find proportion of values between 1-3 
+library(ggplot2)
 
-print(mc_integral) 
-
-# pnorm gives the integral under the Normal distribution (in this case with mean 1 and standard deviation 2) from 
-# negative infinity up to the value specified by q, it replaces the table of probabilites and Z-scores
-mc_exact = pnorm(q=3, mean=1, sd=2) - pnorm(q=1, mean=1, sd=2) 
-print(mc_exact)
-
-# exercise
+# EX(modified)
+reps <- 100 # number of repeats 
+mc_exact = pgamma(q=4, shape=1, scale=2) - pgamma(q=1, shape=1, scale=2) 
 
 accuracy_n <- matrix( 
   integer(10),         # the data elements 
   nrow=5,              # number of rows 
-  ncol=2,              # number of columns 
+  ncol=3,              # number of columns 
   byrow = TRUE)        # fill matrix by rows 
 
-n_samples <- c(100, 1000, 10000, 100000, 1000000)
+n_samples <- c(100, 1000, 10000, 100000, 1000000) # sample sizes
 
 for (r in 1:nrow(accuracy_n)) {
+  mc_integral <- integer(reps) # vector with 100 entries
+  
+  for (rep in 1:reps) {
+    sims <- rgamma(n_samples[r], shape = 1, scale = 2) # simulate n_samples samples from a Gamma distribution 
+                                                       # with above specified properties
+    
+    mc_integral[rep] <- sum(sims >= 1 & sims <= 3)/n_samples[r] # compute proportion of samples in the specified range
+  }
+  
   for (c in 1:ncol(accuracy_n)) { 
     if (c == 1) {
+      # enter sample size into first column of result matrix
       accuracy_n[r,c] <- n_samples[r]
     } else if (c == 2) {
-      sims <- rnorm(n_samples[r], mean = 1, sd = 2) 
-      mc_integral <- sum(sims >= 1 & sims <= 3)/n_samples[r]
-      accuracy_n[r,c] <- mc_integral
+      # enter mean accuracy into second column of result matrix
+      accuracy_n[r,c] <- mean(mc_integral - mc_exact)
+    } else if (c == 3) {
+      # enter sd accuracy into second column of result matrix
+      accuracy_n[r,c] <- sd(mc_integral - mc_exact)
     }
   }
 }
 
-plot(accuracy_n[, 1], accuracy_n[, 2],
-     xlab = "Number of Samples", ylab = "Accuracy",
-     pch = 19, frame = FALSE)
+# make a dataframe out of the columns of the matrix for convenience
+df = data.frame(ssize = accuracy_n[, 1], mean = accuracy_n[, 2], sd = accuracy_n[, 3]) 
+
+ggplot(df, aes(x=ssize, y=mean)) + 
+  geom_line() +
+  geom_point() +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
+                position=position_dodge(0.05)) + 
+  ylab("Estimate-Exact") +
+  xlab("Run")
 
 # Could be improved by running the simulation of a sample of 100/1000/.. multiple times and 
 # plotting their mean and standard deviation
